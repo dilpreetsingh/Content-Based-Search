e3e4702f5d93facdbd7b1f16f8b50caf5c69f0fb
heytitle <pat.chormai@gmail.com>
2018-12-25 11:44:58 +0100

    add densenet

--- src/models.py
+++ src/models.py
@@ -1,7 +1,9 @@
 import torch.nn as nn
 import torchvision.models as tvm
+import torch.nn.functional as F
 
 from torchsummary import summary
+
 
 import utils
 
@@ -11,6 +13,10 @@ def get_model(name):
         return VGG16()
     elif name is 'resnet152':
         return Resnet152()
+    elif name is 'squeezenet':
+        return Squeezenet()
+    elif name is 'densenet':
+        return Densenet()
     else:
         raise SystemExit('Please specify a model name')
 
@@ -20,8 +26,7 @@ class Model:
         summary(self.model, (3, 224, 224))
 
     def forward_pass(self, batch):
-        raise SystemExit('No Implementation')
-
+        return self.features.forward(batch).view(batch.size(0), -1)
 
 class VGG16(Model):
     def __init__(self):
@@ -45,5 +50,26 @@ class Resnet152(Model):
 
         self.features = nn.Sequential(*list(self.model.children())[:-1]).to(utils.torch_device())
 
+
+class Densenet(Model):
+    def __init__(self):
+        super(Densenet, self).__init__()
+
+        self.model = tvm.densenet201(pretrained=True)
+        self.features = self.model.features.to(utils.torch_device())
+
     def forward_pass(self, batch):
-        return self.features.forward(batch).view(batch.size(0), -1)
+        features = self.features.forward(batch)
+        out = F.relu(features, inplace=True)
+        return F.adaptive_avg_pool2d(out, (1, 1)).view(features.size(0), -1)
+
+
+class Squeezenet(Model):
+    def __init__(self):
+        super(Squeezenet, self).__init__()
+
+        self.model = tvm.squeezenet1_1(pretrained=True)
+
+        self.features = nn.Sequential(*list(self.model.children())[:-3]).to(utils.torch_device())
+
+
