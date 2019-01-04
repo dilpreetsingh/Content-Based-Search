2471351d7a499db8320edc6664e0153cd167d032
heytitle <pat.chormai@gmail.com>
2019-01-04 12:20:02 +0100

    add module

---
+++ src/torchx.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Flatten(nn.Module):
+    def __init__(self):
+        super(Flatten, self).__init__()
+
+    def forward(self, x):
+        return x.view(x.size(0), -1)
+
+
+class Reshape(nn.Module):
+    def __init__(self, shape):
+        super(Reshape, self).__init__()
+        self.shape = shape
+
+    def forward(self, x):
+        return x.view(x.size(0), *self.shape)
+
+
+# taken from https://github.com/pytorch/examples/blob/master/vae/main.py
+def vae_loss(recon_x, x, mu, logvar):
+    recon_loss = F.binary_cross_entropy(recon_x, x.view(-1, 28 * 28), reduction='sum')
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return recon_loss + kld
