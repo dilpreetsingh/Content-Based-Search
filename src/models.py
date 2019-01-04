8972b7141fff4dce6f40a18d5a6713bb30219168
heytitle <pat.chormai@gmail.com>
2019-01-04 12:11:24 +0100

    add new models

--- src/models.py
+++ src/models.py
@@ -1,10 +1,12 @@
 import torch.nn as nn
 import torchvision.models as tvm
 import torch.nn.functional as F
+import torch
 
 from torchsummary import summary
 
 
+import torchx
 import utils
 
 
@@ -73,4 +75,58 @@ class Squeezenet(Model):
 
         self.features = nn.Sequential(*list(self.model.children())[:-3]).to(utils.torch_device())
 
+
+class VAE(nn.Module):
+    def __init__(self, latent_dims=2, verbose=False):
+        super(VAE, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 6, 5),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(6, 16, 5),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+            torchx.Flatten(),
+            nn.Linear(16 * 4 * 4, 120),
+            nn.ReLU(True),
+            nn.Linear(120, 2 * latent_dims),
+        )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dims, 120),
+            nn.ReLU(True),
+            nn.Linear(120, 16 * 4 * 4),
+            nn.ReLU(True),
+            torchx.Reshape([16, 4, 4]),
+            nn.Upsample(scale_factor=2),
+            nn.ConvTranspose2d(16, 6, 5),
+            nn.ReLU(True),
+            nn.Upsample(scale_factor=2),
+            nn.ConvTranspose2d(6, 1, 5),
+            nn.Sigmoid()
+        )
+
+        self.loss = torchx.vae_loss
+
+    def forward(self, x):
+        h = self.encoder(x)
+        z_mu, z_logvar = torch.chunk(h, 2, dim=1)
+        z_mu = z_mu.view(z_mu.size(0), -1)
+        z_logvar = z_logvar.view(z_logvar.size(0), -1)
+
+        z_logvar = nn.Softplus()(z_logvar)
+
+        z = self.sampling(z_mu, z_logvar)
+
+        x_hat = self.decoder(z)
+        return x_hat, z, z_mu, z_logvar
+
+    def sampling(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return eps.mul(std).add_(mu)
+
+    def parameters(self):
+        return list(self.encoder.parameters()) + list(self.decoder.parameters())
 
--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,34 @@
 import torch
+import numpy as np
 
 def torch_device():
     if torch.cuda.device_count() > 0:
         return 'cuda:0'
     else:
         return 'cpu'
+
+
+def get_id(path):
+    return path.split('/')[-1].split('.')[0]
+
+
+def get_stats(file_paths, nearest_neighbors, k=3, max_rel_items=5, epsilon=np.finfo(float).eps):
+    num_relevant_items = []
+    for i, path in enumerate(file_paths):
+        if '--m' in path:
+            continue
+
+        id_prefix = '%s--' % get_id(path)
+        sims_path = map(lambda x: file_paths[x], nearest_neighbors[i, 1:(k + 1)])
+        sim_ids = list(filter(lambda s: id_prefix in get_id(s), sims_path))
+
+        num_relevant_items.append(float(len(sim_ids)))
+    num_relevant_items = np.array(num_relevant_items)
+    print('computing stats from %d items' % num_relevant_items.shape[0])
+    precision = num_relevant_items / k
+    recall = num_relevant_items / max_rel_items
+    f1 = 2 * ((precision * recall) / (precision + recall + epsilon))
+
+    print('Precision %.4f +/- %.4f' % (np.mean(precision), np.std(precision)))
+    print('Recall    %.4f +/- %.4f' % (np.mean(recall), np.std(recall)))
+    print('f1    %.4f +/- %.4f' % (np.mean(f1), np.std(f1)))
